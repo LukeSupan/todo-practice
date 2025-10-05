47973a30b4e0355a66e8bc965e6a3b4f8bbd3552
Luke <LukeSupan@outlook.com>
2025-10-05 19:00:36 -0400

    chore: main.js started taking a break before doing this part. that will likely confuse me a bit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// yup
+// index and style were done pretty unguided
+// for this file i followed a tutorial pretty closely
+// whatever i do for practice after this will be unguided
+
+const taskForm = document.querySelector('form');
+const taskInput = document.getElementById('task-input');
+const taskListUL = document.getElementById('tasks-list');
+
+let allTasks = [];
+
+taskForm.addEventListener('submit', function(e) {
+    alert("test");
+})
